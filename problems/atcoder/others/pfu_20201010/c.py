4251567bf4e63828e87b5b0d66f86039b92acf54
Tsubota <kt.tsubo@gmail.com>
2020-10-11 00:54:37 +0900

    Update c.py

--- problems/atcoder/others/pfu_20201010/c.py
+++ problems/atcoder/others/pfu_20201010/c.py
@@ -8,6 +8,6 @@ values = list(range(0, max(p) + 2))
 for i, v in enumerate(p):
     index = bisect.bisect(values, v)
     if value == values[index]:
-        
+        pass
     if i == 0:
         min(ans, value)
