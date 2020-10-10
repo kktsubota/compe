9ebe92f01e8db118f6eaecbda6ade98ea348d847
Tsubota <kt.tsubo@gmail.com>
2020-10-11 01:04:29 +0900

    Update c.py

--- problems/atcoder/others/pfu_20201010/c.py
+++ problems/atcoder/others/pfu_20201010/c.py
@@ -1,13 +1,1 @@
-import bisect
 
-
-n = int(input())
-p = list(map(int, input().split()))
-values = list(range(0, max(p) + 2))
-
-for i, v in enumerate(p):
-    index = bisect.bisect(values, v)
-    if value == values[index]:
-        pass
-    if i == 0:
-        min(ans, value)
